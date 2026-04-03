4ac014cb89355dc139cbcfc418ea296e7281d5a2
balcoder <des.barrett@gmail.com>
2026-04-03 11:32:02 +0100

    signout functionality completed

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -46,6 +46,14 @@ export const signin = async (req, res, next) => {
   }
 };
 
+export const signout = async (req, res, next) => {
+  try {
+    res.clearCookie("access_token");
+    res.status(200).json("User has been logged out!");
+  } catch (error) {
+    next(error);
+  }
+};
 export const google = async (req, res, next) => {
   // req.body sent from client/src/components/OAuth.jsx
   try {
